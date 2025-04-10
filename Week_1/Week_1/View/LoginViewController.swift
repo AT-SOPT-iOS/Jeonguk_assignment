15aabbe3e7272e6d35a40c79c99ae7d95f01fc64
정정욱 <jeonguk29@naver.com>
2025-04-10 23:36:21 +0900

    [Chore] : View 구조 수정

--- Week_1/Week_1/View/LoginViewController.swift
+++ Week_1/Week_1/View/LoginViewController.swift
@@ -9,41 +9,46 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    // MARK: - UIComponent
+    
     let titleLabel: UILabel = {
         let label = UILabel(frame: CGRect(x: 69, y: 161, width: 236, height: 44))
         label.text = "동네라서 가능한 모든것\n당근에서 가까운 이웃과 함께해요."
+        label.font = UIFont.Pretendard.subhead1()
         label.textColor = .black
         label.textAlignment = .center
         label.numberOfLines = 2
-        label.font = UIFont.Pretendard.bold(size: 16)
         return label
     }()
     
     let idTextField: UITextField = {
         let textField = UITextField(frame: CGRect(x: 20, y: 276, width: 335, height: 52))
         textField.placeholder = "아이디"
-        textField.font = .systemFont(ofSize: 14)
-        textField.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
+        textField.font = UIFont.Pretendard.subhead4()
+        textField.backgroundColor = UIColor.Gray200
         return textField
     }()
     
     let passwordTextField: UITextField = {
         let textField = UITextField(frame: CGRect(x: 20, y: 335, width: 335, height: 52))
         textField.placeholder = "비밀번호"
-        textField.font = .systemFont(ofSize: 14)
-        textField.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
+        textField.font = UIFont.Pretendard.subhead4()
+        textField.backgroundColor = UIColor.Gray200
         return textField
     }()
     
     var loginButton: UIButton = {
         let button = UIButton(frame: CGRect(x: 21, y: 422, width: 332, height: 58))
-        button.backgroundColor = UIColor(red: 255/255, green: 111/255, blue: 15/255, alpha: 1)
+        button.backgroundColor = UIColor.Primary_orange
         button.setTitle("로그인하기", for: .normal)
+        button.titleLabel?.font = UIFont.Pretendard.subhead1()
         button.setTitleColor(.white, for: .normal)
         button.titleLabel?.font = .boldSystemFont(ofSize: 18)
         button.addTarget(self, action: #selector(loginButtonDidTap), for: .touchUpInside)
         return button
     }()
+    
+    //MARK: Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,21 +56,16 @@ class LoginViewController: UIViewController {
         addViews()
     }
     
+    // MARK: - Setting
+    
     private func addViews() {
-        view.addSubview(titleLabel)
-        view.addSubview(idTextField)
-        view.addSubview(passwordTextField)
-        view.addSubview(loginButton)
+        view.addSubviews(titleLabel, idTextField, passwordTextField, loginButton)
     }
-    
-    //    private func setLayout() {
-    //        [titleLabel, idTextField, passwordTextField, loginButton].forEach {
-    //            self.view.addSubview($0)
-    //        }
-    //    }
-    
-    
-    //MARK: Action
+}
+
+//MARK: Action
+
+extension LoginViewController {
     @objc
     private func loginButtonDidTap() {
         //presentToWelcomeVC()
@@ -86,7 +86,6 @@ class LoginViewController: UIViewController {
         welcomeViewController.setLabelText(id: idTextField.text)
         self.navigationController?.pushViewController(welcomeViewController, animated: true)
     }
-    
 }
 
 #Preview{
--- Week_1/Week_1/View/WelcomeViewController.swift
+++ Week_1/Week_1/View/WelcomeViewController.swift
@@ -9,28 +9,33 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
     
+    // MARK: - Property
+    
     var id: String?
+    
+    // MARK: - UIComponent
     
     let mainImage: UIImageView = {
         let Image = UIImageView(frame: CGRect(x: 120, y: 60, width: 150, height: 150))
-        Image.image = UIImage(named: "Login")
+        Image.image = ImageLiterals.login
         return Image
     }()
 
     lazy var welcomeLabel: UILabel = {
-        let label = UILabel(frame: CGRect(x: 69, y: 240, width: 236, height: 44))
+        let label = UILabel(frame: CGRect(x: 69, y: 240, width: 236, height: 70))
         label.text = "\(self.id)님\n반가워요"
+        label.font = UIFont.Pretendard.head1()
         label.textColor = .black
         label.textAlignment = .center
         label.numberOfLines = 2
-        label.font = .boldSystemFont(ofSize: 16)
         return label
     }()
     
     var backButton: UIButton = {
         let button = UIButton(frame: CGRect(x: 21, y: 422, width: 332, height: 58))
-        button.backgroundColor = UIColor(red: 255/255, green: 111/255, blue: 15/255, alpha: 1)
+        button.backgroundColor = UIColor.Primary_orange
         button.setTitle("메인으로", for: .normal)
+        button.titleLabel?.font = UIFont.Pretendard.subhead1()
         button.setTitleColor(.white, for: .normal)
         button.titleLabel?.font = .boldSystemFont(ofSize: 18)
         button.addTarget(self, action: #selector(backToLoginButtonDidTap), for: .touchUpInside)
@@ -39,45 +44,52 @@ class WelcomeViewController: UIViewController {
     
     var reLoginButton: UIButton = {
         let button = UIButton(frame: CGRect(x: 21, y: 500, width: 332, height: 58))
-        button.backgroundColor = UIColor(red: 255/255, green: 111/255, blue: 15/255, alpha: 1)
+        button.backgroundColor = UIColor.Gray200
         button.setTitle("다시로그인", for: .normal)
+        button.titleLabel?.font = UIFont.Pretendard.subhead1()
         button.setTitleColor(.white, for: .normal)
         button.titleLabel?.font = .boldSystemFont(ofSize: 18)
         return button
     }()
     
+    //MARK: Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
-        setLayout()
+        addViews()
         //bindID()
         id = ""
     }
     
-    private func setLayout() {
-        [mainImage, welcomeLabel, backButton, reLoginButton].forEach {
-            self.view.addSubview($0)
-        }
+    // MARK: - Setting
+    
+    private func addViews() {
+        view.addSubviews(mainImage, welcomeLabel, backButton, reLoginButton)
     }
+}
+
+//MARK: Action
+
+extension WelcomeViewController{
     
     @objc
     private func backToLoginButtonDidTap() {
-        if self.navigationController == nil { // 모달이라는 이야기
+        if self.navigationController == nil { 
             self.dismiss(animated: true)
         } else {
             self.navigationController?.popViewController(animated: true)
         }
     }
     
-    
     private func bindID() {
         self.welcomeLabel.text = "\(id ?? "")님 \n반가워요!"
     }
     
     func setLabelText(id: String?) {
-        self.id = id // 요놈은 왜 타입 표출이 안일어나지?
+        //self.id = id // 요놈은 왜 타입 표출 일어남? String? 로 선언 되어 있기 때문임
+        self.welcomeLabel.text = "\(id ?? "")님 \n반가워요!"
     }
-    
 }
 
 #Preview{

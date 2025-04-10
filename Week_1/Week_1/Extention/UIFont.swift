60b29e6b5b7021b7f8f6e7b076f6ff1c114dfe63
정정욱 <jeonguk29@naver.com>
2025-04-10 23:35:44 +0900

    [Feat] : 이미지, 아이콘 extension 추가

--- Week_1/Week_1/Extention/UIFont.swift
+++ Week_1/Week_1/Extention/UIFont.swift
@@ -9,27 +9,27 @@ import UIKit
 
 extension UIFont {
     enum Pretendard {
-        static func head1(size: CGFloat) -> UIFont {
+        static func head1() -> UIFont {
             return UIFont(name: "Pretendard-Regular", size: 25)!
         }
         
-        static func subhead1(size: CGFloat) -> UIFont {
+        static func subhead1() -> UIFont {
             return UIFont(name: "Pretendard-Bold", size: 18)!
         }
 
-        static func subhead2(size: CGFloat) -> UIFont {
+        static func subhead2() -> UIFont {
             return UIFont(name: "Pretendard-SemiBold", size: 16)!
         }
         
-        static func subhead3(size: CGFloat) -> UIFont {
+        static func subhead3() -> UIFont {
             return UIFont(name: "Pretendard-SemiBold", size: 15)!
         }
         
-        static func subhead4(size: CGFloat) -> UIFont {
+        static func subhead4() -> UIFont {
             return UIFont(name: "Pretendard-SemiBold", size: 14)!
         }
         
-        static func subhead5(size: CGFloat) -> UIFont {
+        static func subhead5() -> UIFont {
             return UIFont(name: "Pretendard-SemiBold", size: 14)!
         }
     }

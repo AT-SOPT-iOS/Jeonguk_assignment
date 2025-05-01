a3c8217a2c8669a7bad9ea61e84f03a5aecc8dcb
정정욱 <jeonguk29@naver.com>
2025-05-01 23:37:32 +0900

    [Feat] : 3주차 TVING 메인뷰 구현 Resource, Extension 셋팅

---
+++ Week_03/Week_03/Extension/UITextField.swift
@@ -0,0 +1,23 @@
+//
+//  UITextField.swift
+//  Week_03
+//
+//  Created by 정정욱 on 5/1/25.
+//
+
+import UIKit
+
+extension UITextField {
+    
+    func setLeftPadding(amount: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    
+    func setRightPadding(amount: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+}

87a40f1d4dfdb5f7ee51b5d4bfee8205cdf6de73
정정욱 <jeonguk29@naver.com>
2025-05-01 17:36:40 +0900

    [Feat] : 3주차 TVING 메인뷰 구현 초기 셋팅

---
+++ Week_03/Week_03/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week_03
+//
+//  Created by 정정욱 on 5/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

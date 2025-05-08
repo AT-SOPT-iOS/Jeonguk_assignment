c8f671504bf2aac44a392a01b9652954e2899ae2
정정욱 <jeonguk29@naver.com>
2025-05-08 19:09:21 +0900

    [Feat]: 초기 프로젝트 셋팅 (네트워크 포함)

---
+++ Week_04/Week_04/App/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  Week_04
+//
+//  Created by 정정욱 on 5/8/25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let rootViewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = navigationController 
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
+

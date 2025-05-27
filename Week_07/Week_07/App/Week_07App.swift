39e2aebd1f78413b002f9a4485f099d7b43a1fa5
정정욱 <jeonguk29@naver.com>
2025-05-27 21:33:55 +0900

    build: 프로젝트 초기 셋팅

---
+++ Week_07/Week_07/App/Week_07App.swift
@@ -0,0 +1,17 @@
+//
+//  Week_07App.swift
+//  Week_07
+//
+//  Created by 정정욱 on 5/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Week_07App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

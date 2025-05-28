4f48e777965ab379725deacc36e9ae8983b975b7
정정욱 <jeonguk29@naver.com>
2025-05-28 11:25:48 +0900

    feat: SectionHeaderView 구현

---
+++ Week_07/Week_07/View/Header/SectionHeaderView.swift
@@ -0,0 +1,37 @@
+//
+//  SectionHeaderView.swift
+//  Week_07
+//
+//  Created by 정정욱 on 5/28/25.
+//
+
+import SwiftUI
+
+struct SectionHeaderView: View {
+    var moreButtonDisplayed: Bool = true
+    
+    var body: some View {
+        HStack(alignment: .center) {
+            Text("오늘의 티빙 TOP 20")
+                .font(.header)
+                .bold()
+                .foregroundStyle(Color.white)
+            Spacer()
+            
+            if moreButtonDisplayed {
+                Text("더보기")
+                    .font(.button)
+                    .foregroundStyle(Color.gray2)
+            }
+        }
+        .padding(.horizontal, 12)
+        .padding(.top, 9)
+    }
+}
+
+#Preview {
+    SectionHeaderView(moreButtonDisplayed: false)
+        .background(Color.black)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+}
